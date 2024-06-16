e572644beb105a61e667eea3da38975f547f2014
Shawgi Hafiz <shawgichan@gmail.com>
2024-06-16 10:20:15 +0400

    graphql endpoint

--- cmd/server.go
+++ cmd/server.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+	graph_server "github.com/shawgichan/tourist/cmd/graph"
 	db "github.com/shawgichan/tourist/db/sqlc"
 )
 
@@ -26,7 +27,8 @@ func NewServer(store db.Store, pool *pgxpool.Pool) (*Server, error) {
 
 	RouteServer := NewRouteServer(store, router, pool)
 	RouteServer.ApiRoutes()
-
+	graph := graph_server.NewGraphServer(server.Store, server.StoreWithPool, server.Router, server.Pool)
+	graph.GraphServer()
 	log.Println("testing total routes", len(server.Router.Routes()))
 
 	return server, nil
--- graph/places/models_gen.go
+++ graph/places/models_gen.go
@@ -2,11 +2,55 @@
 
 package places
 
+import (
+	"time"
+)
+
+type DateObj struct {
+	From *time.Time `json:"from,omitempty"`
+	To   *time.Time `json:"to,omitempty"`
+}
+
+type DetailsPage struct {
+	ID            *int            `json:"id,omitempty"`
+	Name          *string         `json:"name,omitempty"`
+	Image         *string         `json:"image,omitempty"`
+	Date          *DateObj        `json:"date,omitempty"`
+	Time          *DateObj        `json:"time,omitempty"`
+	Description   *string         `json:"description,omitempty"`
+	Location      *LocationObj    `json:"location,omitempty"`
+	Tags          []*TagObj       `json:"tags,omitempty"`
+	Features      []*FeatureObj   `json:"features,omitempty"`
+	SimilarEvents []*SimEventsObj `json:"similarEvents,omitempty"`
+}
+
+type FeatureObj struct {
+	ID    *int    `json:"id,omitempty"`
+	Icon  *string `json:"icon,omitempty"`
+	Name  *string `json:"name,omitempty"`
+	Value *string `json:"value,omitempty"`
+}
+
+type LocationObj struct {
+	City *string  `json:"city,omitempty"`
+	Lat  *float64 `json:"Lat,omitempty"`
+	Lng  *float64 `json:"Lng,omitempty"`
+}
+
 type Query struct {
 }
 
-type Sample struct {
-	ID      *int    `json:"id,omitempty"`
-	Title   *string `json:"title,omitempty"`
-	TitleAr *string `json:"title_ar,omitempty"`
+type SimEventsObj struct {
+	ID       *int         `json:"id,omitempty"`
+	Name     *string      `json:"name,omitempty"`
+	Image    *string      `json:"image,omitempty"`
+	Location *LocationObj `json:"location,omitempty"`
+	Rating   *float64     `json:"rating,omitempty"`
+	Date     *DateObj     `json:"date,omitempty"`
+}
+
+type TagObj struct {
+	ID    *int    `json:"id,omitempty"`
+	Name  *string `json:"name,omitempty"`
+	Color *string `json:"color,omitempty"`
 }
